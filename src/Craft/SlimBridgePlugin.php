f01a8f621f3a7b32ce103df1b49fa27d42873515
TJ Draper <tj@buzzingpixel.com>
2021-09-27 16:51:52 -0500

    Set up code quality tools

--- src/Craft/SlimBridgePlugin.php
+++ src/Craft/SlimBridgePlugin.php
@@ -1,9 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BuzzingPixel\SlimBridge\Craft;
 
 use craft\base\Plugin;
 
+/**
+ * @psalm-suppress PropertyNotSetInConstructor
+ */
 class SlimBridgePlugin extends Plugin
 {
 }
