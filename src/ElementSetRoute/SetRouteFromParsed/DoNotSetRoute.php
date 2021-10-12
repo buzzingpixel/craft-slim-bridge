7594ae381856e3faf794fdf6d652451ee919f413
TJ Draper <tj@buzzingpixel.com>
2021-10-12 09:18:22 -0500

    Remove errant dump and die

--- src/ElementSetRoute/SetRouteFromParsed/DoNotSetRoute.php
+++ src/ElementSetRoute/SetRouteFromParsed/DoNotSetRoute.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace BuzzingPixel\SlimBridge\ElementSetRoute\SetRouteFromParsed;
 
-use function dd;
-
 class DoNotSetRoute implements SetRouteContract
 {
     public function set(): void
     {
-        // TODO: Implement set() method.
-        dd('DoNotSetRoute');
     }
 }
